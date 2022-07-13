23e4c1004508cc7b7e844fc7e77ad476ad5c95ef
TomasJod <tomasjodko@yahoo.com>
2022-07-13 19:30:21 +0300

    Add Chromedriver, window dimentions, quit option

---
+++ src/test/java/utils/Driver.java
@@ -0,0 +1,24 @@
+package utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Driver {
+    private static ThreadLocal <WebDriver> driver = new ThreadLocal<>();
+    public static WebDriver getDriver() {
+        return driver.get();
+    }
+
+    public static void setDriver() {
+        WebDriverManager.chromedriver().setup();
+        driver.set(new ChromeDriver());
+        driver.get().manage().window().setSize(new Dimension(1920, 1080));
+    }
+
+    public static void closeDriver (){
+        driver.get().quit();
+        driver.remove();
+    }
+}

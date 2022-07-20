a1c5eb9a49221aef86c23002885aee3dfc67c07b
TomasJod <tomasjodko@yahoo.com>
2022-07-20 15:46:57 +0300

    Add test AddToCart

---
+++ src/test/java/pages/humbleBundle/AddToCartTest.java
@@ -0,0 +1,21 @@
+package pages.humbleBundle;
+
+import pages.Common;
+import pages.Locators;
+
+public class AddToCartTest {
+
+    public static void inputTextIntoSearchBox(String expectedMessage ) {
+        pages.Common.sendKeysToElement(
+                Locators.HomePage.inputSearchMessage, expectedMessage);
+    }
+    public static void clickFirstResult() {
+        pages.Common.clickElement(Locators.HomePage.firstResultClick);
+    }
+    public static void clickAddToCart(){
+        pages.Common.clickElement(Locators.HomePage.clickAddToCart);
+    }
+    public static String fieldCartItemTotal(){
+        return Common.getElementText(Locators.HomePage.fieldCartItemNumber);
+    }
+}
---
+++ src/test/java/test/humbleBundle/AddToCartTest.java
@@ -0,0 +1,33 @@
+package test.humbleBundle;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.Common;
+import pages.Locators;
+import test.BaseTest;
+
+public class AddToCartTest extends BaseTest {
+
+    @BeforeMethod
+    @Override
+    public void startUp() {
+        super.startUp();
+        pages.humbleBundle.HomePageSearchViewAll.open();
+    }
+
+    @Test
+
+    public void addToCartCheckTest (){
+        String expectedMessage = "Age of Empires III";
+        String actualMessage;
+
+        pages.humbleBundle.AddToCartTest.inputTextIntoSearchBox(expectedMessage);
+        pages.humbleBundle.AddToCartTest.clickFirstResult();
+        pages.humbleBundle.AddToCartTest.clickAddToCart();
+        actualMessage = pages.humbleBundle.AddToCartTest.fieldCartItemTotal();
+
+        Assert.assertTrue(actualMessage.contains("1"));
+
+    }
+}

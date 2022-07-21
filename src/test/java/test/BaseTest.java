83e02e6fc566ea59dac279b1656e4f5e9a50552b
TomasJod <tomasjodko@yahoo.com>
2022-07-21 12:58:02 +0300

    Add Listeners

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -2,8 +2,11 @@ package test;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import utils.Driver;
+import utils.TestListener;
 
+@Listeners(TestListener.class)
 public class BaseTest {
 
     @BeforeMethod
